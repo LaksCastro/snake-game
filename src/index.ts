c8b27b9846b40d1b5ea20533483cacbbfca55a82
Laks Castro <lakhsmycastro336@gmail.com>
2020-06-06 05:04:03 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,8 @@
 import Game from "./components/game";
 
-window.onload = function initializeGame() {
-  try {
-    // ====================================================================
-    // Create a new game using using a canvas ID as a base
-    // ====================================================================
-    const game = Game("canvas");
+window.onload = function() {
+  const game = Game("canvas");
 
-    // ====================================================================
-    // Will define all game constants according to the viewport size
-    // ====================================================================
-    game.preLoad();
-
-    // ====================================================================
-    // Will start the game (wow, don't tell me)
-    // ====================================================================
-    game.start();
-  } catch (error) {
-    console.log(error);
-    if (error.message === "Game Over...") initializeGame();
-  }
+  game.preLoad();
+  game.start();
 };
